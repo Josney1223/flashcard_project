a76f29850e8a580a7c117d46a2c47b8694c9811e
luc10921 <kenjiuezu3@gmail.com>
2021-06-17 20:10:41 -0300

    comentarios

--- lib/BackEnd/Collection/Collection.dart
+++ lib/BackEnd/Collection/Collection.dart
@@ -9,11 +9,10 @@ import 'SaveLoad/SaveLoadMobile.dart'
 import 'dart:convert';
 part 'Collection.g.dart';
 
+/// Classe que gerencia a coleção dos decks de Flashcards do usuário
 @JsonSerializable(explicitToJson: true)
 class Collection extends ChangeNotifier {
   /*
-    Classe que gerencia a coleção dos decks de Flashcards do usuário (this._decks). 
-
     Métodos:
     -> void createDeck(String deckName)
     -> void deleteDeck(String deckName)
@@ -25,9 +24,14 @@ class Collection extends ChangeNotifier {
     -> int findDeck(String deckName)
   */
 
+  /// A lista contendo todos os decks na coleção
+  @JsonKey(required: true, disallowNullValue: true)
   List<Deck> decks;
+
+  /// Constantes utilizadas para as operações de editar o deck
   static const REMOVER = 0, INSERIR = 1, MODIFICAR = 2;
 
+  /// Método construtor
   Collection({this.decks}) {
     if (this.decks == null) {
       this.decks = [];
@@ -35,9 +39,8 @@ class Collection extends ChangeNotifier {
     loadFile();
   }
 
+  /// Reseta o deck na lista
   void resetDeck(String deckName) {
-    // reseta do deck na lista
-
     int index = findDeck(deckName);
 
     if (index >= 0) {
@@ -46,33 +49,31 @@ class Collection extends ChangeNotifier {
     }
   }
 
+  /// Cria um deck vazio novo para o usuário
   void createDeck(String deckName) {
-    // Cria um deck novo para o usuário utilizando o nome escolhido
-    // o nome escolhido não pode ser repetido
-
-    // Se já não existe um deck com o mesmo nome, adicione no final
+    // O deck é adicionado no final da coleção
     this.decks.add(new Deck(_correctName(deckName)));
     notifyListeners();
   }
 
+  /// Deleta um deck da coleção
   void deleteDeck(String deckName) {
-    // Deleta um deck da coleção
-
     int index = findDeck(deckName);
 
+    // Se o deck existe, então remove da coleção
     if (index >= 0) {
       this.decks.removeAt(index);
       notifyListeners();
     }
   }
 
+  /// Edita um deck da coleção, podendo remover, adicionar ou modificar um
+  /// flashcard
   void editDeck(String deckName, int operacao, Flashcard card,
       {Flashcard replaceCard}) {
-    // Edita um deck da coleção, dando as opções de selecionar, remover,
-    // adicionar ou modificar um flashcard, ou voltar para a coleção
-
     int index = findDeck(deckName);
 
+    // Se o deck existe, então...
     if (index >= 0) {
       switch (operacao) {
         // Remover flashcard
@@ -98,13 +99,14 @@ class Collection extends ChangeNotifier {
           break;
       }
     }
-
     notifyListeners();
   }
 
+  /// Importa um deck, dado uma String
   void importDeck(String jsonDeck) {
     Deck deck;
 
+    // Tenta transformar a String num deck
     try {
       deck = Deck.fromJson(jsonDecode(utf8.decode(jsonDeck.codeUnits)));
     } catch (e) {
@@ -115,7 +117,7 @@ class Collection extends ChangeNotifier {
       }
     }
 
-    // Se existir um deck com os parametros
+    // Se deu para criar um deck, então adiciona no fim da coleção
     if (deck != null) {
       deck.setName(_correctName(deck.getName()));
       this.decks.add(deck);
@@ -125,8 +127,8 @@ class Collection extends ChangeNotifier {
     notifyListeners();
   }
 
+  /// Exporta o deck como uma string que representa o JSON do deck
   String exportDeck(String deckName) {
-    // Chama a função exportDeck da classe ImportExport
     int index = findDeck(deckName);
     String encodedDeck;
 
@@ -139,18 +141,21 @@ class Collection extends ChangeNotifier {
     return encodedDeck;
   }
 
+  /// Salva a coleção no aparelho
   void saveFile() {
-    // Chama a função saveFile da classe SaveLoad
+    // Chama a função save da classe SaveLoadWeb ou SaveLoadMobile
     save(jsonEncode(this.decks));
   }
 
+  /// Carrega a coleção no aparelho
   Future<void> loadFile() async {
-    // Chama a função loadFile da classe SaveLoad
+    // Chama a função load da classe SaveLoadWeb ou SaveLoadMobile
 
     String jsonLoaded = await load();
     if (jsonLoaded == '') {
       notify('Arquivo não encontrado');
     } else {
+      // Se carregou algo, então tenta compreender o JSON
       try {
         List<dynamic> dynamicList =
             jsonDecode(utf8.decode(jsonLoaded.codeUnits));
@@ -167,9 +172,9 @@ class Collection extends ChangeNotifier {
     notifyListeners();
   }
 
+  /// Procura um deck na lista de decks e retorna o index, se não existir então
+  /// a posição -1 é retornada
   int findDeck(String deckName) {
-    // Procura um deck na lista de decks e retorna o index, se não existir então
-    // a posição -1 é retornada
     int index = -1;
 
     int tam = this.decks.length;
@@ -190,9 +195,8 @@ class Collection extends ChangeNotifier {
     return index;
   }
 
+  /// Retorna um deck da lista de decks com o nome deckName
   Deck getDeck(String deckName) {
-    // Retorna um deck da lista de decks com o nome deckName
-
     int index = findDeck(deckName);
 
     if (index >= 0) {
@@ -201,6 +205,7 @@ class Collection extends ChangeNotifier {
     return null;
   }
 
+  /// Atribui um outro nome para um deck
   void setDeckName(String deckName, String newDeckName) {
     int index = findDeck(deckName);
 
@@ -210,10 +215,8 @@ class Collection extends ChangeNotifier {
     notifyListeners();
   }
 
+  /// Verifica se já existe um deck com o nome e se existe então muda o nome
   String _correctName(String nome) {
-    // Verifica se já existe um deck com o nome e existe, então mude o nome
-
-    // Se existir um deck com mesmo nome, então muda o nome
     if (findDeck(nome) >= 0) {
       int copies = 1;
       bool exist = true;
@@ -229,10 +232,11 @@ class Collection extends ChangeNotifier {
     return nome;
   }
 
-  // Funções de Json
+  /// Transforma um Map em uma Collection
   factory Collection.fromJson(Map<String, dynamic> json) =>
       _$CollectionFromJson(json);
 
+  /// Transforma uma Collection em um Map
   Map<String, dynamic> toJson() => _$CollectionToJson(this);
 
   void notify(String error) {
--- lib/BackEnd/Collection/SaveLoad/SaveLoadMobile.dart
+++ lib/BackEnd/Collection/SaveLoad/SaveLoadMobile.dart
@@ -2,24 +2,25 @@ import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
 import 'dart:io';
 
+/// Salva a coleção de decks no celular.
 Future<void> save(String jsonDecks) async {
-  // Salva a coleção de decks no celular
-  await writeOnFile(jsonDecks);
+  await _writeOnFile(jsonDecks);
 }
 
+/// Carrega a coleção de decks que está no celular.
 Future<String> load() async {
   File file = await _localFile;
-  return readFileFromPath(file);
+  return _readFileFromPath(file);
 }
 
-Future<File> writeOnFile(String content) async {
+Future<File> _writeOnFile(String content) async {
+  // Escreve no arquivo
   final file = await _localFile;
 
-  // Escreve no arquivo
   return file.writeAsString(content);
 }
 
-Future<String> readFileFromPath(File file) async {
+Future<String> _readFileFromPath(File file) async {
   try {
     // Le o arquivo
     final contents = await file.readAsString();
@@ -33,13 +34,13 @@ Future<String> readFileFromPath(File file) async {
 }
 
 Future<String> get _localPath async {
-  // Path dos documentos do PC/Android/iOS
+  // Path dos documentos do Android e iOS
   final directory = await getApplicationDocumentsDirectory();
   return directory.path;
 }
 
 Future<File> get _localFile async {
-  // O arquivo que será salvo
+  // O arquivo que será salvo ou lido no celular
   final path = await _localPath;
   final collection = 'KadoCollection.json';
   final completePath = p.join(path, collection);
